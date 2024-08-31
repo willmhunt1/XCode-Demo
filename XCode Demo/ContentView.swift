639fc74283def848fa9d5dee0f2da09ba72d596a
willmhunt1 <willmhunt1@gmail.com>
2024-08-30 23:02:24 -0400

    Added exclamation point

--- XCode Demo/ContentView.swift
+++ XCode Demo/ContentView.swift
@@ -13,12 +13,12 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, world!!")
         }
         .padding()
     }
 }
 
 #Preview {
-    ContentView()
+    ContentView();
 }
