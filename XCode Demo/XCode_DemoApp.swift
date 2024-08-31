b09337944cfa8f2a094fc06895f4812399dd7d06
willmhunt1 <willmhunt1@gmail.com>
2024-08-30 22:56:03 -0400

    Initial Commit

---
+++ XCode Demo/XCode_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCode_DemoApp.swift
+//  XCode Demo
+//
+//  Created by Will Hunt on 8/30/24.
+//
+
+import SwiftUI
+
+@main
+struct XCode_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
